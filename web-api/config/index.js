e72d9ac766b8320b3a612e727250195748218781
benbolthouse <benbolthouse@gmail.com>
2021-07-15 17:53:05 +0000

    Add db section to config

--- web-api/config/index.js
+++ web-api/config/index.js
@@ -15,8 +15,16 @@
  */
 
 module.exports = {
+    env: process.env.NODE_ENV,
     domain: process.env.DOMAIN,
     name: process.env.NAME,
     port: process.env.PORT,
     version: process.env.VERSION,
+    db: {
+        database: process.env.DB_DATABASE,
+        host: process.env.DB_HOST,
+        password: process.env.DB_PASSWORD,
+        port: process.env.DB_PORT,
+        user: process.env.DB_USER,
+    }
 };
