b51cc66523551871c2b4a560ab4aa7670fb1bacc
benbolthouse <benbolthouse@gmail.com>
2021-07-16 20:01:23 +0000

    Implement types and actions for testing

---
+++ web-api/graphql/resolvers/index.js
@@ -0,0 +1,7 @@
+const email = require('./resolvers.email');
+const user = require('./resolvers.user');
+
+module.exports = {
+    email,
+    user,
+};
---
+++ web-api/graphql/resolvers/resolvers.user.js
@@ -0,0 +1,17 @@
+const User = () => ({
+    id: 1,
+    username: "bbolthouse",
+    firstName: "Ben",
+    lastName: "Botlhouse",
+    email: [
+        {
+            id: 2,
+            value: "benbolthouse@gmail.com"
+        }
+    ],
+    createdAt: "NOW",
+    updatedAt: "NOW",
+    revokedAt: null,
+});
+
+module.exports = User;
