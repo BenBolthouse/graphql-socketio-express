2510391f1608268ddcabe9054190015913d7aca5
benbolthouse <benbolthouse@gmail.com>
2021-07-16 19:54:27 +0000

    Create graphql config module

---
+++ web-api/graphql/index.js
@@ -0,0 +1,33 @@
+/**
+ * Container path: /usr/src/app/graphql/index.js
+ * Local path: /graphql/index.js
+ */
+
+/**
+ * Exported object configures the graphql endpoint in server config.
+ */
+
+const { buildSchema } = require('graphql');
+const fs = require('fs');
+const path = require('path');
+
+const config = require('../config');
+const resolvers = require('./resolvers');
+
+// The following extracts a string of types and actions from text files in
+// respective directories. They are parsed by graphql on runtime.
+const typesFilenames = fs.readdirSync(path.join(__dirname, 'types'), 'utf-8');
+const typesFilesString = typesFilenames.reduce((acc, cur) => {
+    return acc += fs.readFileSync(path.join(__dirname, 'types', cur));
+}, '');
+const actionsFilenames = fs.readdirSync(path.join(__dirname, 'actions'), 'utf-8');
+const actionsFilesString = actionsFilenames.reduce((acc, cur) => {
+    return acc += fs.readFileSync(path.join(__dirname, 'actions', cur));
+}, '');
+const schema = buildSchema(typesFilesString + actionsFilesString);
+
+module.exports = {
+    resolvers,
+    schema,
+    graphiql: config.env === 'development',
+};
