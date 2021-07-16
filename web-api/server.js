4a57733825f90470ae5c9b4c069253dcb7293ed6
benbolthouse <benbolthouse@gmail.com>
2021-07-16 19:48:43 +0000

    Add graphql endpoint to server

--- web-api/server.js
+++ web-api/server.js
@@ -3,11 +3,14 @@
  * Project path: /server.js
  */
 
+const { graphqlHTTP } = require('express-graphql');
 const express = require('express');
 const morgan = require('morgan');
 
 const config = require('./config');
 const aboutRoutes = require('./routes/routes.about');
+
+const graphqlConfig = require('./graphql');
 
 /**
  * Express application object with middleware configuration.
@@ -16,6 +19,10 @@ const server = express();
 
 server.use(morgan('dev'));
 
+// GraphQL endpoint
+server.use(`/v${config.version}/graphql`, graphqlHTTP(graphqlConfig));
+
+// About service endpoint
 server.use(`/v${config.version}`, aboutRoutes);
 
 module.exports = server;
